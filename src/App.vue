08834355854b27ecfaf2d554ebc5a117894731fc
rafaelhenrique31 <82546889+rafaelhenrique31@users.noreply.github.com>
2023-10-19 16:25:30 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import HomePage from '../src/pages/Home/HomePage.vue'
+
+</script>
+
+<template>
+
+  <div>
+    <HomePage></HomePage>
+  </div>
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
